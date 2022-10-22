32aa1217e6229cc804249a63ebe742056f8bbdd3
桜華 <huan0016@gmail.com>
2022-10-22 21:47:28 +0800

    feishu signin

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 # -*- coding:utf-8 -*-
-import time, os, shutil, uvicorn, zipfile, ctypes, platform
-from fastapi import FastAPI, Request, Response
+from email import header
+import time, os, shutil, uvicorn, zipfile, ctypes, platform, uuid, requests
+from fastapi import FastAPI, Request, Response, HTTPException, Cookie
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse, HTMLResponse, JSONResponse
+from pydantic import BaseModel
+from auth import Auth
+import random, string
 
 # 检查是否有静态文件夹,没有则并创建
 for item in ['static', 'templates', 'tmps']:
@@ -19,6 +23,175 @@ def read_item(request: Request):
     print(request.path_params)
     return 'HELLO WORLD !'
 
+session_list = {}
+
+class Signin(BaseModel):
+    mobile: str
+    code: str
+
+
+# 获取环境变量值
+APP_ID="cli_a3a8a7faa4b8d00b"
+APP_SECRET="IoyMD1BmClcX4BNbEwF2BdDNIS2lkJYb"
+FEISHU_HOST="https://open.feishu.cn"
+
+
+# 用获取的环境变量初始化免登流程类Auth
+auth = Auth(FEISHU_HOST, APP_ID, APP_SECRET)
+
+
+# 获取 user_access_token
+def get_user_access_token(code):
+    url = "https://open.feishu.cn/open-apis/auth/v3/app_access_token/internal"
+    # "app_id" 和 "app_secret" 位于HTTP请求的请求体
+    req_body = {"app_id": APP_ID, "app_secret": APP_SECRET}
+    response = requests.post(url, req_body)
+    if response.status_code != 200:
+        raise response.raise_for_status()
+    response_dict = response.json()
+    code = response_dict.get("code", -1)
+    if code != 0:
+        raise HTTPException(status_code=code, detail=response_dict.get("msg"))
+    return response_dict.get("app_access_token")
+
+
+# 获取用户信息
+def get_user_info(user_access_token):
+    url = "https://open.feishu.cn/open-apis/authen/v1/user_info"
+    headers = {
+        "Authorization": "Bearer " + user_access_token,
+        "Content-Type": "application/json",
+    }
+    response = requests.get(url=url, headers=headers)
+    response_dict = response.json()
+    print(response_dict)
+    return response_dict.get("data")
+
+
+# 通过手机号获取用户信息
+def get_user_info_mobile(mobile):
+    url = 'https://api-saas.lzhhjs.cn/apaas/openapi/form/smart/62fb0c62e4b0a9d4b9f53a10/query'
+    headers = { 'Authorization': 'rv3lR5AnOsczhmd7rSx1uIyxMeBACe5X', 'Content-Type': 'application/json;charset=UTF-8' }
+    response = requests.post(url=url, headers=headers, json={'tel':mobile})
+    response_dict = response.json()
+    # print(response_dict['result'])
+    # 取 tel 等于 mobile 的
+    user = None
+    for item in response_dict['result']:
+        if item['tel'] == mobile:
+            user = item
+            break
+    return user
+
+
+# 飞书登录
+@app.get("/feishu/callback")
+def feishu_callback(code:str, response:Response):
+    auth.authorize_user_access_token(code)
+    item = auth.get_user_info()
+    print('user_info:', item)
+
+    # 从平台获取用户信息
+    user_info = get_user_info_mobile(item.mobile)
+    print('user_info:', user_info)
+    if user_info is None:
+        raise HTTPException(status_code=401, detail='手机号未注册')
+
+    # 生成 session
+    session = str(uuid.uuid4())
+    session_list[session] = {item.mobile}
+    response.set_cookie(key="session", value=session)
+    return {"msg": "登录成功", "userinfo": user_info}
+
+
+@app.get("/feishu/get_appid")
+def feishu_appid():
+    return { "appid": "cli_a3a8a7faa4b8d00b" }
+
+# 获取账户信息
+@app.get("/sign", summary="获取资料")
+def profile(session:str=Cookie(None)):
+    mobile = session_list.get(session)
+    print("session:", session)
+    print("mobile:", mobile)
+    if mobile is None:
+        return {
+            'online': False,
+            'admin': False,
+            'name':'Lemon',
+            'email':'Lemon@disp.com',
+            'avatar':'https://avatars.githubusercontent.com/u/6184465?v=4',
+        }
+    return {
+        'online': True,
+        'admin': True,
+        'name':'Lemon',
+        'email':'Lemon@disp.com',
+        'avatar':'https://avatars.githubusercontent.com/u/6184465?v=4',
+    }
+
+code_list = {}
+
+# 发送短信验证码
+@app.post("/sign/sendcode", summary="发送短信验证码")
+def send_tpl_sms(mobile:str):
+    # 先判断手机号是否存在(从平台获取)
+    user = get_user_info_mobile(mobile)
+    if user is None:
+        return { 'code': 400, 'message': '手机号不存在' }
+    
+    # 生成随机验证码(6位数)
+    code = ''.join(random.sample(string.digits, 6))
+
+    # 清理过期验证码(5分钟)
+    for key in code_list.keys():
+        if time.time() - code_list[key]['time'] > 300:
+            code_list.pop(key)
+
+    # 存储验证码
+    code_list[mobile] = {
+        'code': code,
+        'time': time.time()
+    }
+
+    # 发送短信验证码
+    url = 'https://sms.yunpian.com/v2/sms/tpl_single_send.json'
+    headers = { "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8" }
+    data = 'apikey=870bb619c2d1ebc3519789a845ba5dca&mobile='+mobile+'&tpl_id=5227126&tpl_value=%2523code%2523%3D'+code
+    response = requests.post(url=url, headers=headers, data=data)
+    response_dict = response.json()
+    print(response_dict)
+    return response_dict
+
+
+# 登录账户(默认数据从平台获取)
+@app.post("/sign", summary="登录账户", status_code=200)
+def signin(item:Signin, response:Response):
+    user_info = get_user_info_mobile(item.mobile)
+    print('user_info:', user_info)
+    
+    # 判断验证码是否正确, 判断验证码是否过期(5分钟)
+    code = code_list.get(item.mobile)
+    if (code is None) or (time.time() - code['time'] > 300):
+        return { 'code': 400, 'message': '验证码已过期' }
+    if code['code'] != item.code:
+        return { 'code': 400, 'message': '验证码错误' }
+
+    # 生成 session
+    session = str(uuid.uuid4())
+    session_list[session] = {item.mobile}
+    response.set_cookie(key="session", value=session)
+    return {"msg": "登录成功", "userinfo": user_info}
+
+
+# 退出登录
+@app.delete("/sign", summary="退出登录")
+def signout(response=Response):
+    response.delete_cookie(key="session")
+    return {"code": 200, "msg": "已退出"}
+
+
+# 模糊检索文件
 @app.get("/search", summary="模糊检索")
 def search_files(name: str):
     # 从 static 文件夹遍历搜索文件或文件夹名中包含搜索词name的文件或文件夹
@@ -38,6 +211,7 @@ def count(path, count: int = 0):
         for each in files:
             count += 1
     return count
+
 
 def size(path):
     m = 0
@@ -199,15 +373,7 @@ async def add_process_time_header(request: Request, call_next):
                     os.remove(dir)
             return JSONResponse({'status': True, 'name': path[-1]})
 
-    ## 静态文件服务(未鉴权)
-    #elif request.url.path.startswith('/static/'):
-    #    return FileResponse(request.url.path[1:])
-    ## 静态文件服务
-    #elif request.url.path.startswith('/favicon.ico'):
-    #    return Response(status_code=404, content='404 Not Found')
-    #    #return FileResponse('static/favicon.ico')
-
-    print('--------------------------------------------')
+    # 中间件预处理完毕执行主体请求
     start_time = time.time()
     response = await call_next(request)
     process_time = time.time() - start_time
